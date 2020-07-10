279acac41a36937c4c94538f9b4f45e101ae5898
maapalif <maapalif20@gmail.com>
2020-07-10 09:58:38 +0700

    add sql

--- application/views/pic.php
+++ application/views/pic.php
@@ -1,7 +1,7 @@
 <div class="container" style="margin-top: 5px; margin-right: 0px; margin-left: 0px; max-width: 500%; font-size: 12px; ">
 	
 	<div>
-		<a role="button" href="<?php echo site_url('apar/save')?>" class="btn btn-primary"><span class="btn-label"><i class="fa fa-plus"></i></span>New</a>
+		<a role="button" href="<?php echo site_url('company/save')?>" class="btn btn-primary"><span class="btn-label"><i class="fa fa-plus"></i></span>New</a>
 		<p>	
 	</div>
 	
@@ -11,12 +11,12 @@
 			    <thead>
 			      <tr style="background-color: #428bca; color: #FFFFFF;">
 			        <th width="1%"><center>No</center></th>
-					<th><center>Nomor APAR</center></th>
-					<th><center>Titik Lokasi</center></th>
-					<th><center>Massa (Kg)</center></th>
-					<th><center>Tgl Expired</center></th>
-					<th><center>Biaya Refill</center></th>
-					<th width="1%"><center>Aksi</center></th>
+					<th><center>Company Name</center></th>
+					<th><center>Company Phone</center></th>
+					<th><center>Company Adress</center></th>
+					<th><center>Company logo</center></th>
+					<th><center>COmpany PIC</center></th>
+					<th width="1%"><center>Act</center></th>
 			      </tr>
 			    </thead>
 			    <tbody>
@@ -24,41 +24,15 @@
 				$i = 1;
 				foreach($data as $h) {	
 
-					$now  = new DateTime();
-
- 					$Exp  = new DateTime($h->a_Exp);
- 					if($Exp != "0000-00-00" || $Exp != NULL){
-						$Due = $now->diff($Exp)->format("%a");
- 					}
- 					else{
- 						$Due = 0;
- 					}
 
 				?>
 					<tr>
 						<td align= "center"><?= $i++; ?></td>
-						<td align= "center"><?= $h->a_APARNo; ?></td>
-						<td align= "center" nowrap><?= $h->a_Loc;?></td>
-				        <td align= "center" nowrap><?= angkakoma($h->a_Weight); ?></td>
-				        <td align= "center" nowrap style="
-				        <?php
-				        	if($Due <= 40 && $Due != 0){
-							echo"background-color: #DC143C;";
-							echo"font-weight: bold;";
-							}
-							else
-							{
-								echo"font-weight: bold;";
-							}
-				        ?>"><?php
-				        	if($h->a_Exp == '0000-00-00'){
-				        		echo " - ";
-				        	}
-				        	else{
-								echo longdate_indo($h->a_Exp) ;
-				        	}
-				        ?></td>
-				        <td align= "center" nowrap><?= $h->a_Price;?></td>
+						<td align= "center"><?= $h->name; ?></td>
+						<td align= "center" nowrap><?= $h->phone;?></td>
+				        <td align= "center" nowrap><?= $h->address; ?></td>
+				        <td align= "center" nowrap><?= $h->logo; ?></td>
+				        <td align= "center" nowrap><?= $h->pic;?></td>
 				        <td align= "center">
 							<a href="<?php echo site_url('apar/edit')?>/<?= $h->ID ?>" title="Edit"><i class="fa fa-fw fa-pencil-square" style="color: #000000;"></i></a>
 							<a href="<?php echo site_url('apar/delete')?>/<?= $h->ID ?>" title="Delete" onclick="return confirm('Are you sure you want to delete?')"><i class="fa fa-fw fa-window-close" style="color: #000000;"></i></a>
---
+++ sambros_test_adriansyah.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 10 Jul 2020 pada 04.56
+-- Versi server: 10.4.13-MariaDB
+-- Versi PHP: 7.2.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sambros_test_adriansyah`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `company`
+--
+
+CREATE TABLE `company` (
+  `id` int(11) NOT NULL,
+  `name` varchar(35) NOT NULL,
+  `phone` varchar(14) NOT NULL,
+  `address` text NOT NULL,
+  `logo` varchar(50) NOT NULL,
+  `id_PIC` int(11) NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `company`
+--
+
+INSERT INTO `company` (`id`, `name`, `phone`, `address`, `logo`, `id_PIC`, `createdAt`) VALUES
+(1, 'PT. A', '021345678', 'Tangerang', 'logo.jpg', 1, '2020-07-10 02:54:48'),
+(2, 'PT. B', '021678908', 'Tangerang Selatan', 'logob.jpg', 2, '2020-07-10 02:54:48');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pic`
+--
+
+CREATE TABLE `pic` (
+  `id_PIC` int(11) NOT NULL,
+  `name_PIC` varchar(35) NOT NULL,
+  `email_PIC` varchar(50) NOT NULL,
+  `phone_PIC` varchar(14) NOT NULL,
+  `address_PIC` text NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pic`
+--
+
+INSERT INTO `pic` (`id_PIC`, `name_PIC`, `email_PIC`, `phone_PIC`, `address_PIC`, `createdAt`) VALUES
+(1, 'A', 'a@gmail.com', '08764378324', 'Tangerang', '2020-07-10 02:56:20'),
+(2, 'B', 'b@gmail.com', '086514256721', 'BSD', '2020-07-10 02:56:20');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `company`
+--
+ALTER TABLE `company`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `pic`
+--
+ALTER TABLE `pic`
+  ADD PRIMARY KEY (`id_PIC`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `company`
+--
+ALTER TABLE `company`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `pic`
+--
+ALTER TABLE `pic`
+  MODIFY `id_PIC` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
